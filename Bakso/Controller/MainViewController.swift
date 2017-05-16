21501c5bd25d9285fffe90a4def8d8ca2ccaa924
Endimionx <endimion7@gmail.com>
2017-05-17 04:45:30 +0700

    add

---
+++ Bakso/Controller/MainViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  SlideMenuControllerSwift
+//
+//  Created by Yuji Hato on 12/3/14.
+//
+
+import UIKit
+import SlideMenuControllerSwift
+import XLPagerTabStrip
+
+class MainViewController: ButtonBarPagerTabStripViewController {
+    @IBOutlet weak var viewHead: UIView!
+
+    var isReload = false
+   
+    var mainContens = ["data1", "data2", "data3", "data4", "data5", "data6", "data7", "data8", "data9", "data10", "data11", "data12", "data13", "data14", "data15"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set up style before super view did load is executed
+        settings.style.buttonBarBackgroundColor = .clear
+        settings.style.selectedBarBackgroundColor = UIColor(hex: "c8e8ff")
+        settings.style.buttonBarItemsWidth = Int(self.view.frame.width / CGFloat(3))
+        
+        let longTitleLabel = UILabel()
+        longTitleLabel.text = "J-Delivery"
+        longTitleLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        longTitleLabel.sizeToFit()
+        
+        let leftItem = UIBarButtonItem(customView: longTitleLabel)
+        self.navigationItem.leftBarButtonItem = leftItem
+        
+        
+        buttonBarView.removeFromSuperview()
+        //navigationController?.navigationBar.addSubview(buttonBarView)
+        
+        buttonBarView.backgroundColor = UIColor(hex: "0080c1")
+        self.viewHead.backgroundColor = UIColor(hex: "0080c1")
+        self.viewHead.addSubview(buttonBarView)
+        
+        
+        
+        changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            
+            oldCell?.label.textColor = UIColor(white: 1, alpha: 0.6)
+            newCell?.label.textColor = .white
+            
+            if animated {
+                UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                    newCell?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                    oldCell?.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                })
+            }
+            else {
+                newCell?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                oldCell?.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            }
+        }
+        
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setNavigationBarItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let child_1 = ChildMenuReguler(itemInfo: "BAKSO REGULER")
+        let child_2 = ChildMenuFrozen(itemInfo: "FROZEN FOOD")
+        let child_3 = ChildMenuComing(itemInfo: "COMING SOON")
+        
+        guard isReload else {
+            return [child_1, child_2, child_3]
+        }
+        
+        var childViewControllers = [child_1, child_2, child_3]
+        
+        for (index, _) in childViewControllers.enumerated(){
+            let nElements = childViewControllers.count - index
+            let n = (Int(arc4random()) % nElements) + index
+            if n != index{
+                swap(&childViewControllers[index], &childViewControllers[n])
+            }
+        }
+        let nItems = 1 + (arc4random() % 3)
+        return Array(childViewControllers.prefix(Int(nItems)))
+    }
+    
+    override func reloadPagerTabStripView() {
+        isReload = true
+        if arc4random() % 2 == 0 {
+            pagerBehaviour = .progressive(skipIntermediateViewControllers: arc4random() % 2 == 0, elasticIndicatorLimit: arc4random() % 2 == 0 )
+        }
+        else {
+            pagerBehaviour = .common(skipIntermediateViewControllers: arc4random() % 2 == 0)
+        }
+        super.reloadPagerTabStripView()
+    }
+    
+    override func configureCell(_ cell: ButtonBarViewCell, indicatorInfo: IndicatorInfo) {
+        super.configureCell(cell, indicatorInfo: indicatorInfo)
+        cell.backgroundColor = .clear
+    }
+
+}
+//huhuhu
+
+
+extension MainViewController : SlideMenuControllerDelegate {
+    
+    func leftWillOpen() {
+        print("SlideMenuControllerDelegate: leftWillOpen")
+    }
+    
+    func leftDidOpen() {
+        print("SlideMenuControllerDelegate: leftDidOpen")
+    }
+    
+    func leftWillClose() {
+        print("SlideMenuControllerDelegate: leftWillClose")
+    }
+    
+    func leftDidClose() {
+        print("SlideMenuControllerDelegate: leftDidClose")
+    }
+    
+    func rightWillOpen() {
+        print("SlideMenuControllerDelegate: rightWillOpen")
+    }
+    
+    func rightDidOpen() {
+        print("SlideMenuControllerDelegate: rightDidOpen")
+    }
+    
+    func rightWillClose() {
+        print("SlideMenuControllerDelegate: rightWillClose")
+    }
+    
+    func rightDidClose() {
+        print("SlideMenuControllerDelegate: rightDidClose")
+    }
+}
